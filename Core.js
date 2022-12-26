d5b8b8650d8d2569f959cbd95b06b07819e02a3a
Bajrang11yt <121365871+Bajrang11yt@users.noreply.github.com>
2022-12-26 22:45:50 +0530

    Update Core.js

--- Core.js
+++ Core.js
@@ -5461,7 +5461,27 @@ Miku.sendMessage(`${ownertag}@s.whatsapp.net`, util.format(err), {quoted:m})
 console.log(err)
 }
 }
+pattern: "fancy",
+            desc: "Makes stylish/fancy given text",
+            category: "converter",
+            use: '56 Secktor',
+            react: "✅",
+            filename: __filename
+        },
 
+const { sck1, tiny, fancytext, listall,cmd } = require('../lib/')
+let fancytextt = await fancytext(`${text.slice(2)}`, text.split(" ")[0])
+            citel.reply(fancytextt)
+        async(Void, citel, text) => {
+            if (isNaN(text.split(" ")[0]) || !text) {
+                let text = tiny(
+                    "Fancy text generator\n\nExample: .fancy 32 Secktor\n\n"
+                );
+                listall("Secktor Bot").forEach((txt, num) => {
+                    text += `${(num += 1)} ${txt}\n`;
+                });
+                return await citel.reply(text);
+            }
 
 let file = require.resolve(__filename)
 fs.watchFile(file, () => {
